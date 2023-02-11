407b2417fef77967091ae17c2c80065a65c79785
vaibhav <ankitkumar35546567@gmail.com>
2023-02-11 13:33:19 +0530

    final chnages

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,12 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public Boat(){}
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     public String getName() {
         return name;
     }
